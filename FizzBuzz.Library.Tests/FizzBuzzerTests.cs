f7dbccbeda6a4e7f98be0ea1e68f57f4bcee8b60
Chandra Sekhar Gandi <chandugsekhar@hotmail.com>
2017-04-16 18:58:16 -0400

    Initial project

---
+++ FizzBuzz.Library.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Library.Tests
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        [Test]
+        public void Buzzer_WhenDefault_ReturnsInput(
+            [Values(1, 2, 4, 7, 8)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual(input.ToString(), output);
+        }
+
+        [Test]
+        public void Buzzer_When3_ReturnsFizz(
+            [Values(3, 6, 9)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Fizz", output);
+        }
+         
+        [Test]
+        public void Buzzer_When5_ReturnsBuzz(
+            [Values(5, 10)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Buzz", output);
+        }
+
+        [Test]
+        public void Buzzer_WhenDivisibleBy3And5_ReturnsFizzBuzz(
+            [Values(15)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("FizzBuzz", output);
+        }
+    }
+}
